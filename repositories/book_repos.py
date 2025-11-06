2c186301ef7c402fc5e6784eda547c3e91d81d54
denissragalj-private <denis.sragalj@gmail.com>
2025-11-06 21:04:50 +0100

    Update book_repos.py

--- repositories/book_repos.py
+++ repositories/book_repos.py
@@ -10,6 +10,10 @@ VALUES (?, ?, ?, ?, ?)
 '''
 
 
+
+
+
+
 def add_book(book: Book) -> int:
     if isinstance(book, Book):
         params = (book.title, book.description, book.isbn, book.price, book.author.id)
@@ -23,3 +27,22 @@ def add_book(book: Book) -> int:
             return cursor.lastrowid
     except Exception as ex:
         print(f'Dogodila se greska {ex}.')
+
+
+def get_book(id: int) -> Book:
+    # 1. dohvati knjigu iz baze po id-u
+    try:
+        with sqlite3.connect(DB_PATH) as conn:
+            cursor = conn.cursor()
+            cursor.execute('SELECT * FROM book WHERE id = ?', (id,))
+            row = cursor.fetchone()
+            if row:
+                return Book(*row)
+            return None
+    except Exception as ex:
+        print(f'Dogodila se greska {ex}.')
+
+
+def delete_book(id:int):
+    # 1. dohvati knjigu iz baze po id-u
+    # 2. ako knjiga postoji, obrisati je iz baze
