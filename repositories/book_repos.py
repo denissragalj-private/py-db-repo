bb30c174180bb903b58fed0402ef264c0eb93d8e
Denis Šragalj <denis.sragalj@gmail.com>
2025-11-07 12:37:12 +0100

    popravio

--- repositories/book_repos.py
+++ repositories/book_repos.py
@@ -4,14 +4,10 @@ from constants import DB_PATH
 from models.books import Book
 
 
-sql_create_book = '''
+sql_create_book = """
 INSERT INTO book (title, description, isbn, price, author_id)
 VALUES (?, ?, ?, ?, ?)
-'''
-
-
-
-
+"""
 
 
 def add_book(book: Book) -> int:
@@ -26,7 +22,7 @@ def add_book(book: Book) -> int:
             cursor.execute(sql_create_book, params)
             return cursor.lastrowid
     except Exception as ex:
-        print(f'Dogodila se greska {ex}.')
+        print(f"Dogodila se greska {ex}.")
 
 
 def get_book(id: int) -> Book:
@@ -34,15 +30,16 @@ def get_book(id: int) -> Book:
     try:
         with sqlite3.connect(DB_PATH) as conn:
             cursor = conn.cursor()
-            cursor.execute('SELECT * FROM book WHERE id = ?', (id,))
+            cursor.execute("SELECT * FROM book WHERE id = ?", (id,))
             row = cursor.fetchone()
             if row:
                 return Book(*row)
             return None
     except Exception as ex:
-        print(f'Dogodila se greska {ex}.')
+        print(f"Dogodila se greska {ex}.")
 
 
-def delete_book(id:int):
+def delete_book(id: int):
+    pass
     # 1. dohvati knjigu iz baze po id-u
     # 2. ako knjiga postoji, obrisati je iz baze
