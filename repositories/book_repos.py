ca05d207b42ef7457f39c7e8c4cac58ddc8bc8c7
denissragalj-private <denis.sragalj@gmail.com>
2025-11-08 15:04:10 +0100

    uređeno od profesora

--- repositories/book_repos.py
+++ repositories/book_repos.py
@@ -4,10 +4,18 @@ from constants import DB_PATH
 from models.books import Book
 
 
-sql_create_book = """
+sql_create_book = '''
 INSERT INTO book (title, description, isbn, price, author_id)
 VALUES (?, ?, ?, ?, ?)
-"""
+'''
+sql_get_book = '''
+SELECT * FROM book
+WHERE id = ?
+'''
+sql_delete_book = '''
+DELETE FROM book
+WHERE id = ?
+'''
 
 
 def add_book(book: Book) -> int:
@@ -22,23 +30,36 @@ def add_book(book: Book) -> int:
             cursor.execute(sql_create_book, params)
             return cursor.lastrowid
     except Exception as ex:
-        print(f"Dogodila se greska {ex}.")
+        print(f'Dogodila se greska {ex}.')
 
 
 def get_book(id) -> Book:
     try:
         with sqlite3.connect(DB_PATH) as conn:
             cursor = conn.cursor()
-            cursor.execute("SELECT * FROM book WHERE id = ?", (id,))
-            row = cursor.fetchone()
-            if row:
-                return Book(*row)
-            return None
+            cursor.execute(sql_get_book, (id,))
+            book = cursor.fetchone()
+            # medu korak dohvatiti autora iz baze i napraviti objekt Author
+            # kada su svi podaci spremni napraviti objekt Book i vratiit ga pomocu return
+
     except Exception as ex:
-        print(f"Dogodila se greska {ex}.")
+        print(f'Dogodila se greska {ex}.')
 
 
-def delete_book(id: int):
-    pass
-    # 1. dohvati knjigu iz baze po id-u
-    # 2. ako knjiga postoji, obrisati je iz baze
+def delete_book(id: int) -> str:
+    # 1. dohvatiti knjigu iz baze koja ima dobiveni ID
+    book_from_db = get_book(id)
+
+    # 2. ako postoji knjiga u bazi izbrisi je i vrati poruku OK
+    if book_from_db is not None:
+        try:
+            with sqlite3.connect(DB_PATH) as conn:
+                cursor = conn.cursor()
+                cursor.execute(sql_delete_book, (id,))
+                return 'OK'
+        except Exception as ex:
+            print(f'Dogodila se greska {ex}.')
+
+    # ako ne postoji vratiti poruku da nema takve knjige u bazi
+    else:
+        return f'Ne postoji trazena knjiga u bazi!'
